9eef123911c2349d1091380b88381db554711bbc
Marcus Moreira <marcus@quave.com.br>
2024-09-16 22:26:35 -0300

    Fix double rendering

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,9 @@
 'use client'
 
-import { AuthProvider, useAuth } from '@/context/AuthContext';
-import Layout from './layout';
 import GenerateResumePage from './generate-resume/page';
 
 export default function HomePage() {
-  const { loading } = useAuth();
-
-  if (loading) {
-    return <div>Loading...</div>;
-  }
-
   return (
-    <AuthProvider>
-      <Layout>
         <GenerateResumePage />
-      </Layout>
-    </AuthProvider>
   );
 }
