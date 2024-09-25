1914f190378f186857b916d725fcbfa9589aaf8f
Marcus Moreira <marcus@quave.com.br>
2024-09-25 06:55:01 -0300

    Changed profile page layout when no profile

--- app/profile-manager/page.tsx
+++ app/profile-manager/page.tsx
@@ -7,7 +7,7 @@ import { Input } from "@/components/ui/input"
 import { Textarea } from "@/components/ui/textarea"
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
-import { Plus, Pencil, Check, X, Link as LinkIcon, Trash2 } from 'lucide-react'
+import { Plus, Pencil, Check, X, Link as LinkIcon, Trash2, UserX } from 'lucide-react'
 import ProfileWizardComponent from '@/components/ProfileCreationDialog'
 import { useRouter } from 'next/navigation'
 import { AlertDialog, AlertDialogAction, AlertDialogCancel, AlertDialogContent, AlertDialogDescription, AlertDialogFooter, AlertDialogHeader, AlertDialogTitle, AlertDialogTrigger } from "@/components/ui/alert-dialog"
@@ -130,10 +130,16 @@ export default function ProfileManagement() {
     <div className="flex-1 p-4 md:p-8 overflow-auto flex items-center justify-center">
       <Card className="w-full max-w-3xl p-4 md:p-6">
     <div className="container mx-auto p-4 space-y-6">
-      <h1 className="text-3xl font-bold mb-6">Página de Perfis</h1>
+      <h1 className="text-3xl font-bold mb-6">Meus Perfis</h1>
       { profiles.length === 0 ? (
-        <div className="flex justify-center items-center">
-          <p className="text-lg text-muted-foreground">Nenhum perfil encontrado. Clique no botão abaixo para adicionar um novo perfil.</p>
+        <div className="flex justify-center items-center flex-col">
+          <UserX className='h-20 w-20 text-gray-300 mb-4'/>
+          <p className="text-2xl text-gray-400">Voce ainda não tem nenhum perfil cadastrado.</p>
+          <p className="text-base text-gray-600 mb-4">Cadastre seu primeiro perfil para poder começar a criar seus currículos.</p>
+          <Button onClick={quota === 0 ? handleUpgradePlan : handleAddProfile} className='mt-4 mb-6'>
+              <Plus className="h-4 w-4 md:mr-2" /> 
+              <p className="hidden md:block">Perfil</p>
+          </Button>
         </div>
       ) : (
       <Tabs value={activeProfile} onValueChange={setActiveProfile} className="w-full" key={refreshKey}>
