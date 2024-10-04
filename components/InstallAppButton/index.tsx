6629ca6785edf80f4246b4feb503b42e9ceadd4c
Marcus Moreira <marcusmoreira81@gmail.com>
2024-10-03 21:49:30 -0300

    InstallApp Button

--- components/InstallAppButton/index.tsx
+++ components/InstallAppButton/index.tsx
@@ -1,5 +1,3 @@
-'use client'
-
 import React, { useState, useEffect } from 'react';
 import { Button } from '../ui/button';
 
@@ -9,14 +7,17 @@ interface BeforeInstallPromptEvent extends Event {
 }
 
 const InstallAppButton: React.FC = () => {
+  const [isIOS, setIsIOS] = useState(false);
   const [deferredPrompt, setDeferredPrompt] = useState<BeforeInstallPromptEvent | null>(null);
-  const [isInstallable, setIsInstallable] = useState(false);
 
   useEffect(() => {
+    // Verifica se o dispositivo é iOS
+    //eslint-disable-next-line
+    setIsIOS(/iPad|iPhone|iPod/.test(navigator.userAgent) && !(window as any).MSStream);
+
     const handleBeforeInstallPrompt = (e: BeforeInstallPromptEvent) => {
       e.preventDefault();
       setDeferredPrompt(e);
-      setIsInstallable(true);
     };
 
     window.addEventListener('beforeinstallprompt', handleBeforeInstallPrompt as EventListener);
@@ -31,17 +32,29 @@ const InstallAppButton: React.FC = () => {
       deferredPrompt.prompt();
       deferredPrompt.userChoice.then((choiceResult) => {
         if (choiceResult.outcome === 'accepted') {
-          console.log('User accepted the install prompt');
+          console.log('Usuário aceitou o prompt de instalação');
         } else {
-          console.log('User dismissed the install prompt');
+          console.log('Usuário recusou o prompt de instalação');
         }
         setDeferredPrompt(null);
-        setIsInstallable(false);
       });
     }
   };
 
-  if (!isInstallable) {
+  if (isIOS) {
+    return (
+      <div className="p-4 bg-gray-100 rounded-lg shadow-sm">
+        <h3 className="text-lg font-semibold mb-2">Para instalar este app no seu iPhone:</h3>
+        <ol className="list-decimal list-inside space-y-2">
+          <li>Toque no botão de compartilhar no Safari</li>
+          <li>Role para baixo e toque em  `&#34;`Adicionar à Tela de Início `&#34;`</li>
+          <li>Toque em  `&#34;`Adicionar `&#34;` no canto superior direito</li>
+        </ol>
+      </div>
+    );
+  }
+
+  if (!deferredPrompt) {
     return null;
   }
 
