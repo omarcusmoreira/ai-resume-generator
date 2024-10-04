ca87d35f686cfa35a5e79f70f6a819b71cb3480b
Marcus Moreira <marcusmoreira81@gmail.com>
2024-10-03 21:36:00 -0300

    InstallApp Button

--- components/InstallAppButton/index.tsx
+++ components/InstallAppButton/index.tsx
@@ -1,27 +1,12 @@
 'use client';
 
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { Button } from '../ui/button';
 import { BeforeInstallPromptEvent } from '@/types';
 
 const InstallAppButton: React.FC = () => {
   const [deferredPrompt, setDeferredPrompt] = useState<BeforeInstallPromptEvent | null>(null);
-  const [isInstallable, setIsInstallable] = useState(false);
 
-  useEffect(() => {
-    const handleBeforeInstallPrompt = (event: Event) => {
-      event.preventDefault();
-      const promptEvent = event as BeforeInstallPromptEvent;
-      setDeferredPrompt(promptEvent);
-      setIsInstallable(true);
-    };
-
-    window.addEventListener('beforeinstallprompt', handleBeforeInstallPrompt);
-
-    return () => {
-      window.removeEventListener('beforeinstallprompt', handleBeforeInstallPrompt);
-    };
-  }, []);
 
   const handleInstallClick = () => {
     if (deferredPrompt) {
@@ -33,14 +18,9 @@ const InstallAppButton: React.FC = () => {
           console.log('User dismissed the install prompt');
         }
         setDeferredPrompt(null);
-        setIsInstallable(false);
       });
     }
   };
-
-  if (!isInstallable) {
-    return null;
-  }
 
   return (
     <Button variant='ai' onClick={handleInstallClick} className="install-button">
